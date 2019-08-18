a74d6a868e88f6d358db9c3cb8afca5c821312df
kirkdc <kirk_dcosta@yahoo.com>
2019-08-18 19:47:57 +0530

    basic auth plugged in

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "544887809849-jok6v1jgfq4gosmru98u5cqb0f6ds19h.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
   render() {
     return <div> Google Auth</div>;
   }
